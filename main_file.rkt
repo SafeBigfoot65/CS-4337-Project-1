facd4a525cbf5a9727e10398590bd1e1b3fd11d0
SafeBigfoot65 <tobennanwosu10@gmail.com>
2025-10-02 09:18:32 -0500

    Change file type of "main_file" from python to racket

--- main_file.rkt
+++ main_file.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+
 ; THIS IS FOR MY USE ONLY: I JUST WANT TO BE ABLE TO EASILY COPY THE FUNCTION CALL(S) IN THE TERMINAL WITHOUT HAVING TO WRITE IT MANUALLY OVER AND OVER AGAIN!!!!
 ; Test case: (infix_to_prefix_conversion "5 * ($1 + $2) + 9")
 ; Test case (if history exists): (evaluate (infix_to_prefix_conversion "5 * ($1 + $2) + 9"))
@@ -34,5 +35,3 @@
       )
     )
   )
-
-
